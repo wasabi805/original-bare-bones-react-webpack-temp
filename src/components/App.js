e93752efc0f0f0d90de613052e5882806b96dd1b
‘Tim <timothy.j.ocampo@gmail.com>
2018-07-24 11:35:09 -0700

    This is the original repo used to make forks

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+//THIS IS THE ORIGINAL FILE USED TO MAKE FORK TEMPLATES
+class App extends Component{
+    render(){
+        return(
+            <div>
+                Coma estas beaches!
+            </div>
+        )
+    }
+}
+
+export default App
